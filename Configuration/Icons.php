5f2fd14debbf3cbf33843ed524c893a89dc7f533
janbager <jan.bager@gmail.com>
2025-01-21 13:35:49 +0100

    fix: icon configuration

---
+++ Configuration/Icons.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    // Icon identifier
+    'tx-hio_typo3_connector-publication' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/publish-svgrepo-com.svg',
+    ],
+];
