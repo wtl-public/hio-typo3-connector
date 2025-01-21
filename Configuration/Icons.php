e3139edb2ac654934126e92a56a739311550ee5e
janbager <jan.bager@gmail.com>
2025-01-21 14:26:57 +0100

    fix: icon for publication

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,7 +7,7 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     'tx-hio_typo3_connector-publication' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/publish-svgrepo-com.svg',
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/book-svgrepo-com.svg',
     ],
     'tx-hio_typo3_connector-project' => [
         'provider' => SvgIconProvider::class,
