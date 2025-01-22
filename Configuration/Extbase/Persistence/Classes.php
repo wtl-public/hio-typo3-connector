44fab46d7141f946fd98053921b20ad0ca0c324c
janbager <jan.bager@gmail.com>
2025-01-22 15:21:12 +0100

    feat: added fluid templates for patent list

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -2,16 +2,4 @@
 
 declare(strict_types=1);
 
-return [
-/*
-    \Wtl\HioTypo3Connector\Domain\Model\Publication::class => [
-        'tableName' => 'tx_hiotypo3connector_domain_model_publication',
-        'recordType' => \Wtl\HioTypo3Connector\Domain\Model\Publication::class,
-        'properties' => [
-            'objectId' => [
-                'fieldName' => 'object_id',
-            ],
-        ],
-    ],
-*/
-];
+return [];
