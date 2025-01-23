cbb5e2a7298623f215512748283ecf82bb95cfa2
janbager <jan.bager@gmail.com>
2025-01-23 09:45:43 +0100

    fix: missing frontend plugin config

--- ext_localconf.php
+++ ext_localconf.php
@@ -86,5 +86,15 @@ ExtensionUtility::configurePlugin(
     pluginType:  ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 // non-cacheable actions
 );
+ExtensionUtility::configurePlugin(
+// extension name, matching the PHP namespaces (but without the vendor)
+    'HioTypo3Connector',
+    // arbitrary, but unique plugin name (not visible in the backend)
+    'PersonSelectedPatentList',
+    // all actions
+    [PersonController::class => 'patentList'],
+    pluginType:  ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+// non-cacheable actions
+);
 
 
