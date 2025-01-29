765793d2818ae6b6682deb1c82ad391df080f30d
janbager <jan.bager@gmail.com>
2025-01-29 14:31:31 +0100

    feat: new icons for plugins

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,16 +5,36 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-    'tx-hio_typo3_connector-publication' => [
+    'tx-hio_typo3_connector-list-of-publications' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/book-svgrepo-com.svg',
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-of-publications.svg',
     ],
-    'tx-hio_typo3_connector-project' => [
+    'tx-hio_typo3_connector-list-of-projects' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/project-svgrepo-com.svg',
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-of-projects.svg',
     ],
-    'tx-hio_typo3_connector-list' => [
+    'tx-hio_typo3_connector-list-of-persons' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-svgrepo-com.svg',
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-of-persons.svg',
+    ],
+    'tx-hio_typo3_connector-list-of-patents' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-of-patents.svg',
+    ],
+    'tx-hio_typo3_connector-publications' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/publication.svg',
+    ],
+    'tx-hio_typo3_connector-projects' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/project.svg',
+    ],
+    'tx-hio_typo3_connector-persons' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/person.svg',
+    ],
+    'tx-hio_typo3_connector-patents' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/patent.svg',
     ],
 ];
