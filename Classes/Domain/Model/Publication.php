9df7fbfcc886ae43b128f19e15edadffce4cf703
janbager <jan.bager@gmail.com>
2025-04-14 11:36:41 +0200

    Revert "feat: removed uid prop from domain model"
    This reverts commit 44fd36e44b35f2cd9f79187f67efdb70332d523a.

--- Classes/Domain/Model/Publication.php
+++ Classes/Domain/Model/Publication.php
@@ -8,6 +8,7 @@ use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 class Publication extends AbstractEntity
 {
+    protected int $uid;
     protected int $objectId = 0;
 
     protected string $title = '';
