faf07fc4c53f30f60e6901c968b139784cc11a78
janbager <jan.bager@gmail.com>
2025-04-14 09:48:29 +0200

    feat: change upsert check in publication repository

--- Classes/Domain/Repository/PublicationRepository.php
+++ Classes/Domain/Repository/PublicationRepository.php
@@ -11,15 +11,16 @@ class PublicationRepository extends BaseRepository
     public function savePublications($publications, $storagePageId): void {
         /** @var PublicationDTO $publication */
         foreach ($publications as $publication) {
-            $publicationModel = $this->findOneBy(['objectId' => $publication->getObjectId()]);
+            $publicationModel = $this->findByUid($publication->getObjectId());
             if ($publicationModel === null) {
                 $publicationModel = new Publication();
+                $publicationModel->setUid($publication->getObjectId());
+                $publicationModel->setPid($storagePageId);
                 $publicationModel->setObjectId($publication->getObjectId());
                 $publicationModel->setTitle($publication->getTitle());
                 $publicationModel->setType($publication->getType());
                 $publicationModel->setDetails($publication->getDetails());
                 $publicationModel->setSearchIndex($publication->getSearchIndex());
-                $publicationModel->setPid($storagePageId);
                 $publicationModel->setReleaseYear($publication->getReleaseYear());
 
                 $this->add($publicationModel);
