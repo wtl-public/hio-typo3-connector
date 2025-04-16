32292ecfe576d78e3e4f72eb0053656985919ad4
janbager <jan.bager@gmail.com>
2025-04-16 10:43:20 +0200

    feat: added request publication command and api client

---
+++ Classes/Command/RequestPublicationImportCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wtl\HioTypo3Connector\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Wtl\HioTypo3Connector\Command\ConfigureRequestImportTrait;
+use Wtl\HioTypo3Connector\Services\HioMiddlewareRequestImportService;
+
+class RequestPublicationImportCommand extends Command
+{
+    use ConfigureRequestImportTrait;
+    protected const REQUESTED_ENTITY_TYPE = 'publication';
+
+    protected static $defaultName = 'hio:request:publication:import';
+
+    public function __construct(
+        private readonly HioMiddlewareRequestImportService $hioMiddlewareRequestImportService,
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->hioMiddlewareRequestImportService->configure(
+            $input->getArgument('api-url'),
+            $input->getArgument('api-username'),
+            $input->getArgument('api-password'),
+            $input->getOption('api-verify-ssl'),
+            $input->getArgument('t3-webhook-url'),
+            $input->getArgument('t3-x-api-key'),
+            $input->getArgument('t3-batch-size'),
+        );
+
+        $response = $this->hioMiddlewareRequestImportService->requestImport(self::REQUESTED_ENTITY_TYPE);
+        if ($response->getStatusCode() !== 200) {
+            $output->writeln('Error requesting data import from HIO Middleware API: ' . $response->getBody());
+            return Command::FAILURE;
+        }
+        $output->writeln('Data import requested successfully.');
+        return Command::SUCCESS;
+    }
+}
---
+++ Classes/Services/HioMiddlewareRequestImportService.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Wtl\HioTypo3Connector\Services;
+
+use TYPO3\CMS\Core\Http\RequestFactory;
+use TYPO3\CMS\Core\Log\Logger;
+
+class HioMiddlewareRequestImportService
+{
+    protected array $headers;
+
+    protected string $url;
+
+    private RequestFactory $requestFactory;
+
+    private Logger $logger;
+
+    /**
+     * @var array
+     */
+    protected $auth = [];
+
+    protected $verifySsl = false;
+
+    /**
+     * @var int
+     */
+    protected $batchSize = 100;
+
+    public function __construct(RequestFactory $requestFactory)
+    {
+        $this->headers = ['Content-Type' => 'application/json'];
+        $this->requestFactory = $requestFactory;
+        $this->logger = new Logger('HioApiService');
+        $this->url = ''; // configure this in the scheduler task
+
+        $this->batchSize = 100; // configure this in the scheduler task
+        $this->webhookUrl = ''; // configure this in the scheduler task
+        $this->xApiKey = ''; // configure this in the scheduler task
+    }
+
+    public function configure(
+        string $apiUrl,
+        string $apiUsername,
+        string $apiPassword,
+        bool $verifySsl,
+        string $webhookUrl,
+        string $xApiKey,
+        int $batchSize
+    ): void
+    {
+        $this->url = $apiUrl;
+        if ($apiUsername && $apiPassword) {
+            $this->auth = [$apiUsername, $apiPassword];
+        }
+        $this->verifySsl = $verifySsl;
+        $this->webhookUrl = $webhookUrl;
+        $this->xApiKey = $xApiKey;
+        $this->batchSize = $batchSize;
+    }
+
+    public function requestImport(string $entityType): ApiResponse{
+        try {
+            $response = $this->requestFactory->request($this->url, 'POST', [
+                'headers' => $this->headers,
+                'auth' => $this->auth,
+                'verify' => $this->verifySsl,
+                'json' => [
+                    'type' => $entityType,
+                    'webhookUrl' => $this->webhookUrl,
+                    'xApiKey' => $this->xApiKey,
+                    'batchSize' => $this->batchSize,
+                ]
+            ]);
+            return $response;
+
+        } catch (\Exception $e) {
+            $this->logger->error(sprintf(
+                'Error requesting data import from HIO Middleware API: %s',
+                $e->getMessage()
+            ));
+
+            throw($e);
+        }
+    }
+}
