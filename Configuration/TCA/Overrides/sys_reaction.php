bb53e5ae6469b4e4a296199dc86777ca78759a64
janbager <jan.bager@gmail.com>
2025-04-16 12:59:46 +0200

    feat: added storage pid to reaction

--- Configuration/TCA/Overrides/sys_reaction.php
+++ Configuration/TCA/Overrides/sys_reaction.php
@@ -5,6 +5,23 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') or die();
 
 if (ExtensionManagementUtility::isLoaded('reactions')) {
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'sys_reaction',
+        [
+            'storage_pid' => [
+                'label' => 'LLL:EXT:reactions/Resources/Private/Language/locallang_db.xlf:sys_reaction.storage_pid',
+                'description' => 'LLL:EXT:reactions/Resources/Private/Language/locallang_db.xlf:sys_reaction.storage_pid.description',
+                'config' => [
+                    'type' => 'group',
+                    'allowed' => 'pages',
+                    'size' => 1,
+                    'maxitems' => 1,
+                ],
+            ],
+        ]
+    );
+
     // Add the custom type to the type select
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
         'sys_reaction',
