6f1a3244e29808d080e0f3954fa7040dc4aab064
janbager <jan.bager@gmail.com>
2025-04-16 10:54:51 +0200

    fix: changed argument order for import command

--- Classes/Command/ConfigureRequestImportTrait.php
+++ Classes/Command/ConfigureRequestImportTrait.php
@@ -16,6 +16,19 @@ trait ConfigureRequestImportTrait
                 InputArgument::REQUIRED,
                 'required argument: Middleware API-Endpoint URl'
             )->addArgument(
+                't3-webhook-url',
+                InputArgument::REQUIRED,
+                'required argument: TYPO3 webhook Url (the url to receive publications)'
+            )->addArgument(
+                't3-x-api-key',
+                InputArgument::REQUIRED,
+                'required argument: x-api-key'
+            )->addArgument(
+                't3-batch-size',
+                InputArgument::REQUIRED,
+                'optional argument: batch size (default: 100)',
+                100
+            )->addArgument(
                 'api-username',
                 InputArgument::OPTIONAL,
                 'optional argument: basic auth username'
@@ -28,18 +41,6 @@ trait ConfigureRequestImportTrait
                 null,
                 InputOption::VALUE_NONE,
                 'optional argument: verify API ssl certificate (default: false)'
-            )->addArgument(
-                't3-webhook-url',
-                InputArgument::REQUIRED,
-                'required argument: TYPO3 webhook Url (the url to receive publications)'
-            )->addArgument(
-                't3-x-api-key',
-                InputArgument::REQUIRED,
-                'required argument: x-api-key'
-            )->addArgument(
-                't3-batch-size',
-                InputArgument::OPTIONAL,
-                'optional argument: batch size (default: 100)'
             );
     }
 }
