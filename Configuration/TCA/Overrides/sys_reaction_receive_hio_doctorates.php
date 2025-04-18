5796f1a0176cb73222e5ae0e7b4c3386a1753985
janbager <jan.bager@gmail.com>
2025-04-18 19:39:22 +0200

    feat: added remaining reactions for receiving data

---
+++ Configuration/TCA/Overrides/sys_reaction_receive_hio_doctorates.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+if (ExtensionManagementUtility::isLoaded('reactions')) {
+
+    // Add the custom type to the type select
+    ExtensionManagementUtility::addTcaSelectItem(
+        'sys_reaction',
+        'reaction_type',
+        [
+            \Wtl\HioTypo3Connector\Reaction\ReceiveHioDoctoratesReaction::getDescription(),
+            \Wtl\HioTypo3Connector\Reaction\ReceiveHioDoctoratesReaction::getType(),
+            \Wtl\HioTypo3Connector\Reaction\ReceiveHioDoctoratesReaction::getIconIdentifier(),
+        ]
+    );
+
+    $GLOBALS['TCA']['sys_reaction']['types'][\Wtl\HioTypo3Connector\Reaction\ReceiveHioDoctoratesReaction::getType()] = [
+        'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;config,
+        --palette--;;createRecord,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;access',
+    ];
+
+}
