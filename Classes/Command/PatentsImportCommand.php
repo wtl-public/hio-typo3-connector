a110743ae7582b36bba00c4b04776246086dac4e
janbager <jan.bager@gmail.com>
2025-04-18 17:40:14 +0200

    feat: changed import commands to use events

--- Classes/Command/PatentsImportCommand.php
+++ Classes/Command/PatentsImportCommand.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Wtl\HioTypo3Connector\Command;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
-use Wtl\HioTypo3Connector\Domain\Repository\PatentRepository;
+use Wtl\HioTypo3Connector\Event\ReceiveHioPatentEvent;
 use Wtl\HioTypo3Connector\Services\HioPatentService;
 use Wtl\HioTypo3Connector\Trait\WithConfigureImportCommandTrait;
 
@@ -20,9 +21,9 @@ class PatentsImportCommand extends Command
     protected static $defaultName = 'hio:patents:import';
 
     public function __construct(
-        private readonly PatentRepository $patentRepository,
         private readonly HioPatentService $hioPatentService,
-        protected readonly PersistenceManager $persistenceManager)
+        protected readonly EventDispatcherInterface $eventDispatcher
+    )
     {
         parent::__construct();
     }
@@ -42,18 +43,20 @@ class PatentsImportCommand extends Command
             (bool)$input->getOption('verify-ssl')
         );
 
-        $querySettings = $this->patentRepository->createQuery()->getQuerySettings()->setStoragePageIds([$input->getArgument('storagePageId')]);
-        $this->patentRepository->setDefaultQuerySettings($querySettings);
-
         $currentPage = 1;
         do {
+            /** @var PatentDto[] $patents */
             $patents = $this->hioPatentService->getPatents($currentPage);
             if ($this->hioPatentService->getMeta()->getTotal() === 0) {
                 $output->writeln('No new patents found');
                 return Command::SUCCESS;
             }
 
-            $this->patentRepository->savePatents($patents, $input->getArgument('storagePageId'));
+            foreach ($patents as $patent) {
+                $this->eventDispatcher->dispatch(
+                    new ReceiveHioPatentEvent($patent, $input->getArgument('storagePageId'))
+                );
+            }
 
             $currentPage++;
         } while ($currentPage <= $this->hioPatentService->getMeta()->getLastPage());
--- Classes/Command/ProjectsImportCommand.php
+++ Classes/Command/ProjectsImportCommand.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Wtl\HioTypo3Connector\Command;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
-use Wtl\HioTypo3Connector\Domain\Repository\ProjectRepository;
+use Wtl\HioTypo3Connector\Event\ReceiveHioProjectEvent;
 use Wtl\HioTypo3Connector\Services\HioProjectService;
 use Wtl\HioTypo3Connector\Trait\WithConfigureImportCommandTrait;
 
@@ -20,9 +21,8 @@ class ProjectsImportCommand extends Command
     protected static $defaultName = 'hio:projects:import';
 
     public function __construct(
-        private readonly ProjectRepository $projectRepository,
         private readonly HioProjectService $hioProjectService,
-        protected readonly PersistenceManager $persistenceManager)
+        protected readonly EventDispatcherInterface $eventDispatcher)
     {
         parent::__construct();
     }
@@ -42,18 +42,20 @@ class ProjectsImportCommand extends Command
             (bool)$input->getOption('verify-ssl')
         );
 
-        $querySettings = $this->projectRepository->createQuery()->getQuerySettings()->setStoragePageIds([$input->getArgument('storagePageId')]);
-        $this->projectRepository->setDefaultQuerySettings($querySettings);
-
         $currentPage = 1;
         do {
+            /** @var ProjectDto[] $projects */
             $projects = $this->hioProjectService->getProjects($currentPage);
             if ($this->hioProjectService->getMeta()->getTotal() === 0) {
                 $output->writeln('No new projects found');
                 return Command::SUCCESS;
             }
 
-            $this->projectRepository->saveProjects($projects, $input->getArgument('storagePageId'));
+            foreach ($projects as $project) {
+                $this->eventDispatcher->dispatch(
+                    new ReceiveHioProjectEvent($project, $input->getArgument('storagePageId'))
+                );
+            }
 
             $currentPage++;
         } while ($currentPage <= $this->hioProjectService->getMeta()->getLastPage());
