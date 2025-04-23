c02fc1b7024f5ff51df4eed1a95c41a087f06bdc
janbager <jan.bager@gmail.com>
2025-04-23 14:53:11 +0200

    feat: added AttachHioPatentToHioPersonsEvent

--- Classes/EventListener/ReceiveHioPatentListener.php
+++ Classes/EventListener/ReceiveHioPatentListener.php
@@ -4,19 +4,33 @@ declare(strict_types=1);
 
 namespace Wtl\HioTypo3Connector\EventListener;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Wtl\HioTypo3Connector\Domain\Repository\PatentRepository;
+use Wtl\HioTypo3Connector\Event\AttachHioPatentToHioPersonsEvent;
 use Wtl\HioTypo3Connector\Event\ReceiveHioPatentEvent;
 
 class ReceiveHioPatentListener
 {
-    protected ?PatentRepository $patentRepository = null;
-
-    public function injectPatentRepository(PatentRepository $patentRepository): void
+    public function __construct(
+        protected readonly PatentRepository $patentRepository,
+        protected readonly EventDispatcherInterface $eventDispatcher
+    )
     {
-        $this->patentRepository = $patentRepository;
     }
+
     public function __invoke(ReceiveHioPatentEvent $event): void
     {
         $this->patentRepository->save($event->getHioPatent(), $event->getStoragePid());
+        $hioPatentObjectId = $event->getHioPatent()->getObjectId();
+        $hioPersonObjectIds = array_map(
+            static fn($hioPerson) => $hioPerson->getId(),
+            $event->getHioPatent()->getPersons() ?? []
+        );
+        $this->eventDispatcher->dispatch(
+            new AttachHioPatentToHioPersonsEvent(
+                $hioPatentObjectId,
+                $hioPersonObjectIds
+            )
+        );
     }
 }
