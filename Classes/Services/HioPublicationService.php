ea9410c8193c8196ad94b5f520dec56d04ca78c6
janbager <jan.bager@gmail.com>
2025-04-24 09:15:37 +0200

    fix: import of publications

--- Classes/Services/HioPublicationService.php
+++ Classes/Services/HioPublicationService.php
@@ -18,6 +18,9 @@ class HioPublicationService extends HioApiService
         $result = $apiResponse->getData();
         foreach ($result as $publication) {
             $releaseYear = $publication['journal']['releaseYear'] ?? null;
+            if ($releaseYear !== null) {
+                $releaseYear = (int)$releaseYear;
+            }
             $dto = PublicationDto::fromArray($publication);
             $dto->setReleaseYear($releaseYear);
             $publications[] = $dto;
