6c8c7e7c601dba3e2a8ac9a35f3f58abc48ced42
janbager <jan.bager@gmail.com>
2025-05-27 14:24:00 +0200

    fix: added version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,6 +3,7 @@
 $EM_CONF["hio_typo3_connector"] = [
     'title' => 'hio-typo3-connector',
     'description' => 'TYPO3 Connector for HISinOne',
+    'version' => '0.7.2',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.0',
