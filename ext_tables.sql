5f1cd43b399c5a0bf2c34175573f6f334664fca6
janbager <jan.bager@gmail.com>
2025-06-27 11:31:20 +0200

    fix: change title field type to LONGTEXT in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE tx_hiotypo3connector_domain_model_project
 CREATE TABLE tx_hiotypo3connector_domain_model_publication
 (
     object_id    INT(11)  DEFAULT '0' NOT NULL,
-    title        VARCHAR(255) DEFAULT '',
+    title     LONGTEXT DEFAULT '',
     details      MEDIUMBLOB,
     search_index MEDIUMBLOB,
     type         VARCHAR(255) DEFAULT '',
