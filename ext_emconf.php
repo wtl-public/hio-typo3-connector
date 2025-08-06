b86066c147380535b8a364dff5cdec769b39d7b6
eissmann <eissmann@users.noreply.github.com>
2025-08-06 14:00:25 +0000

    chore: Sync from development release 0.8.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF["hio_typo3_connector"] = [
     'title' => 'TYPO3 Connector für HISinOne',
     'description' => 'TYPO3 Connector für die Integration von Daten aus HISinOne',
-    'version' => '0.7.50',
+    'version' => '0.8.0',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
