54b315f96e8824c915439786152f3061e9cb156a
Steffen Eißmann <steffen.eissmann@wtl.de>
2025-08-06 14:55:50 +0200

    Delete hio-release-repo directory

--- hio-release-repo/Configuration/TCA/Overrides/pages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-ExtensionManagementUtility::registerPageTSConfigFile(
-    'hio_typo3_connector',
-    'Configuration/TsConfig/Page/Mod/Wizards/HioTypo3Connector.tsconfig',
-    'HIO TYPO3 Connector Plugins',
-);
